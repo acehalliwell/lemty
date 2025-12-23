0b3b53fab2418f072a103691c588df5a4ad801d5
acehalliwell <acehalliwell@gmail.com>
2025-12-24 01:15:12 +0800

    Connect to Pgsql

---
+++ lemty-ui/scripts/Connection.php
@@ -0,0 +1,60 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+
+/**
+ * Represent the Connection
+ */
+class Connection {
+    /**
+     * Connection
+     * @var type
+     */
+    private static $conn;
+    /**
+     * Connect to the database and return an instance of \PDO object
+     * @return \PDO
+     * @throws \Exception
+     */
+    public function connect() {
+
+        // read parameters in the ini configuration file
+        $params = [
+            'host' => $_ENV['POSTGRES_HOST'],
+            'port' => $_ENV['POSTGRES_PORT'],
+            'database' => $_ENV['POSTGRES_DB'],
+            'user' => $_ENV['POSTGRES_USER'],
+            'password' => $_ENV['POSTGRES_PWD']
+        ];
+
+        // connect to the postgresql database
+        $conStr = sprintf("pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
+                $params['host'],
+                $params['port'],
+                $params['database'],
+                $params['user'],
+                $params['password']);
+        $pdo = new \PDO($conStr);
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+    /**
+     * return an instance of the Connection object
+     * @return type
+     */
+    public static function get() {
+        if (null === static::$conn) {
+            static::$conn = new static();
+        }
+        return static::$conn;
+    }
+    protected function __construct() {
+    }
+    private function __clone() {
+    }
+    public function __wakeup() {
+    }
+}
---
+++ lemty-ui/scripts/test.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require_once 'Connection.php';
+
+try {
+    Connection::get()->connect();
+    echo 'A connection to the PostgreSQL database sever has been established successfully.';
+} catch (\PDOException $e) {
+    echo $e->getMessage();
+}
